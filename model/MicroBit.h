1dfb6b59d353778f432321cc2f556f9c6f1ef351
James Devine <j.devine@lancaster.ac.uk>
2019-11-18 19:14:28 +0000

    manually include MESEvents for now
    Because we're not running the bluetooth stack, certain includes that are depended upon are not present.

--- model/MicroBit.h
+++ model/MicroBit.h
@@ -63,6 +63,8 @@ DEALINGS IN THE SOFTWARE.
 #include "MicroBitDevice.h"
 #include "MicroBitCompat.h"
 
+#include "MESEvents.h"
+
 //#include "MicroBitBLEManager.h"
 //#include "MicroBitStorage.h"
 //#include "MicroBitLightSensor.h"
