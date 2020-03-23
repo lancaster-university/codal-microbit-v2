f0c331406ed4fe7a30539bb925dcb184b44f4d50
Raphael Gault <raphael.gault@arm.com>
2020-03-23 11:02:01 +0000

    Motion Sensor: Add a delay at accelerometer/compass detection
    The LSM303 Motion sensor needs at least 6.4ms before it's ready to use.
    This patch adds a delay at the MicroBitAccelerometer and MicroBitCompass
    autodetect.
    
    Signed-off-by: Raphael Gault <raphael.gault@arm.com>

--- source/MicroBitCompass.cpp
+++ source/MicroBitCompass.cpp
@@ -55,6 +55,11 @@ MicroBitCompass::MicroBitCompass(MicroBitI2C &i2c, uint16_t id) : Compass(MicroB
  */
 Compass& MicroBitCompass::autoDetect(MicroBitI2C &i2c)
 {
+    /*
+     * In essence, the LSM needs at least 6.4ms from power-up before we can use it.
+     * https://github.com/microbit-foundation/codal-microbit-next/issues/33
+     */
+    target_wait(10);
     // We only have combined sensors, so rely on the acceleromter detection code to also detect the correct magnetomter.
     if (MicroBitCompass::detectedCompass == NULL)
         MicroBitAccelerometer::autoDetect(i2c);
