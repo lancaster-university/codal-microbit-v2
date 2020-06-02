6534b17c290e3b7d7a27857d0a3b7debb886186e
martinwork <martinwilliamswork@gmail.com>
2020-06-02 21:31:21 +0100

    Adjust GATT table size and RAM region to support all services

--- sdk_config/sdk_config.h
+++ sdk_config/sdk_config.h
@@ -4519,7 +4519,7 @@
 
 // <o> NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE - Attribute Table size in bytes. The size must be a multiple of 4. 
 #ifndef NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE
-#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 1408
+#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 0x720
 #endif
 
 // <o> NRF_SDH_BLE_VS_UUID_COUNT - The number of vendor-specific UUIDs. 
