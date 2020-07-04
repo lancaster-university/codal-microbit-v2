a3f0bcb178814e03b5ff19411dc92e0a77209b41
martinwork <martinwilliamswork@gmail.com>
2020-07-04 21:53:10 +0100

    microbit_panic and target_panic

--- source/MicroBitDevice.cpp
+++ source/MicroBitDevice.cpp
@@ -25,6 +25,7 @@ DEALINGS IN THE SOFTWARE.
 #include "MicroBitConfig.h"
 #include "MicroBitDevice.h"
 #include "nrf.h"
+#include "hal/nrf_gpio.h"
 
 #if CONFIG_ENABLED(MICROBIT_BLE_ENABLED)
 #include "nrf_sdm.h"
@@ -80,41 +81,6 @@ int microbit_random(int max)
     return microbit_device_instance ? microbit_device_instance->random(max) : 0;
 }
 
-static void wait_cycles(uint32_t cycles)
-{
-    __asm__ __volatile__(".syntax unified\n"
-                         "1: subs %0, #1\n"
-                         "bne 1b\n"
-                         : "+r"(cycles)
-                         :
-                         :);
-}
-
-// TODO make this actually display the error code on the screen
-// for now it just blinks
-void microbit_panic(int statusCode)
-{
-    target_disable_irq();
-    DMESG("*** CODAL PANIC : [%d]", statusCode);
-    NRF_P0->OUT = 0;
-    NRF_P1->OUT = 0;
-    NRF_P0->DIR = 0;
-    NRF_P1->DIR = 0;
-    NRF_P0->PIN_CNF[21] = 0x303;
-    NRF_P1->PIN_CNF[5] = 0x303;
-    while (1)
-    {
-        NRF_P0->OUT = 0xffffffff;
-        wait_cycles(1 << 21);
-        NRF_P0->OUT = 0;
-        wait_cycles(1 << 21);
-    }
-}
-
-void microbit_panic_timeout(int iterations)
-{
-    //TODO: Populate this once a graphical panic is implemented
-}
 
 /**
  * Derive a unique, consistent serial number of this device from internal data.
@@ -245,4 +211,244 @@ void microbit_seed_random(uint32_t seed)
     random_value = seed;
 }
 
+
+// LED matrix map for use by microbit_panic
+
+typedef uint8_t microbit_LEDMapPinNumber;
+
+typedef int (*microbit_LEDMapFnRCtoXY)( int row, int col);
+
+typedef struct microbit_LEDMapStr
+{
+    int         width;                      // The physical width of the LED matrix, in pixels.
+    int         height;                     // The physical height of the LED matrix, in pixels.
+    int         rows;                       // The number of drive pins connected to LEDs.
+    int         columns;                    // The number of sink pins connected to the LEDs.
+
+    const microbit_LEDMapPinNumber *rowPins;      // Array of pin numbers to drive.
+    const microbit_LEDMapPinNumber *columnPins;   // Array of pin numbers to sink.
+    
+    microbit_LEDMapFnRCtoXY mapRCtoX;       // Map logical LED positions to physical positions
+    microbit_LEDMapFnRCtoXY mapRCtoY;
+} microbit_LEDMapStr;
+
+
+// LED matrix map values for micro:bit V2
+
+// Width and height of LEDs
+#ifndef microbit_LEDMap_WIDTH
+#define microbit_LEDMap_WIDTH 5
+#endif
+
+#ifndef microbit_LEDMap_HEIGHT
+#define microbit_LEDMap_HEIGHT 5
+#endif
+
+// Width and height of LED pins
+#ifndef microbit_LEDMap_COLS
+#define microbit_LEDMap_COLS 5
+#endif
+
+#ifndef microbit_LEDMap_ROWS
+#define microbit_LEDMap_ROWS 5
+#endif
+
+// LED rows pinmap
+#ifndef microbit_LEDMap_PINROW0
+#define microbit_LEDMap_PINROW0 21
+#endif
+
+#ifndef microbit_LEDMap_PINROW1
+#define microbit_LEDMap_PINROW1 22
+#endif
+
+#ifndef microbit_LEDMap_PINROW2
+#define microbit_LEDMap_PINROW2 15
+#endif
+
+#ifndef microbit_LEDMap_PINROW3
+#define microbit_LEDMap_PINROW3 24
+#endif
+
+#ifndef microbit_LEDMap_PINROW4
+#define microbit_LEDMap_PINROW4 19
+#endif
+
+// LED columns pinmap
+#ifndef microbit_LEDMap_PINCOL0
+#define microbit_LEDMap_PINCOL0 28
+#endif
+
+#ifndef microbit_LEDMap_PINCOL1
+#define microbit_LEDMap_PINCOL1 11
+#endif
+
+#ifndef microbit_LEDMap_PINCOL2
+#define microbit_LEDMap_PINCOL2 31
+#endif
+
+#ifndef microbit_LEDMap_PINCOL3
+#define microbit_LEDMap_PINCOL3 37
+#endif
+
+#ifndef microbit_LEDMap_PINCOL4
+#define microbit_LEDMap_PINCOL4 30
+#endif
+
+static const microbit_LEDMapPinNumber microbit_LEDMap_rowPins[ microbit_LEDMap_ROWS] =
+{
+    microbit_LEDMap_PINROW0, microbit_LEDMap_PINROW1, microbit_LEDMap_PINROW2, microbit_LEDMap_PINROW3, microbit_LEDMap_PINROW4
+};
+
+static const microbit_LEDMapPinNumber microbit_LEDMap_columnPins[ microbit_LEDMap_COLS] =
+{
+    microbit_LEDMap_PINCOL0, microbit_LEDMap_PINCOL1, microbit_LEDMap_PINCOL2, microbit_LEDMap_PINCOL3, microbit_LEDMap_PINCOL4
+};
+
+static int microbit_LEDMap_RCtoX( int row, int col) { return col; }
+static int microbit_LEDMap_RCtoY( int row, int col) { return row; }
+
+static const microbit_LEDMapStr microbit_LEDMap =
+{
+    microbit_LEDMap_WIDTH, microbit_LEDMap_HEIGHT,
+    microbit_LEDMap_COLS, microbit_LEDMap_ROWS,
+    microbit_LEDMap_rowPins,
+    microbit_LEDMap_columnPins,
+    microbit_LEDMap_RCtoX,
+    microbit_LEDMap_RCtoY
+};
+
+
+// Low level LEDs pin functions
+
+static inline void microbit_LEDMap_rowOn(        int row)           { nrf_gpio_pin_set(   microbit_LEDMap.rowPins[ row]); }
+static inline void microbit_LEDMap_rowOff(       int row)           { nrf_gpio_pin_clear( microbit_LEDMap.rowPins[ row]); }
+
+static inline void microbit_LEDMap_columnOn(     int col, bool on)  { nrf_gpio_pin_write( microbit_LEDMap.columnPins[ col], on ? 0 : 1); }
+static inline void microbit_LEDMap_columnOff(    int col)           { nrf_gpio_pin_set(   microbit_LEDMap.columnPins[ col]); }
+
+static inline void microbit_LEDMap_pinOutput(    uint8_t pin)       { nrf_gpio_cfg_output( pin); }
+
+static inline void microbit_LEDMap_configure()
+{
+    for ( int i = 0; i < microbit_LEDMap.columns; i++)  microbit_LEDMap_pinOutput( microbit_LEDMap.columnPins[i]);
+    for ( int i = 0; i < microbit_LEDMap.rows;    i++)  microbit_LEDMap_pinOutput( microbit_LEDMap.rowPins[i]);
+}
+
+
+// Constants for microbit_panic
+
+// length of message: face, E, code digit, code digit, code digit
+#ifndef microbit_panic_MSGLEN
+#define microbit_panic_MSGLEN         5
+#endif
+
+// position of first code digit
+#ifndef microbit_panic_MSG1STDIGIT
+#define microbit_panic_MSG1STDIGIT    2
+#endif
+
+// divisor for first digit
+#ifndef microbit_panic_DIVMAX
+#define microbit_panic_DIVMAX         100
+#endif
+
+// divisor base
+#ifndef microbit_panic_DIVBASE
+#define microbit_panic_DIVBASE        10
+#endif
+
+// Number of burn passes for each character
+#ifndef microbit_panic_SCANS
+#define microbit_panic_SCANS         40
+#endif
+
+// Passes left clear for repeated character
+#ifndef microbit_panic_CLEARSCANS
+#define microbit_panic_CLEARSCANS    3
+#endif
+
+// Delay cycles for each row
+#ifndef microbit_panic_ROWDELAY
+#define microbit_panic_ROWDELAY       (40000)
+#endif
+
+static const uint8_t panic_face[ MICROBIT_FONT_WIDTH] = { 0x1B, 0x1B, 0x0, 0x0E, 0x11};
+
+static int panic_timeout = 0;
+
+
+void microbit_panic_timeout(int iterations)
+{
+    panic_timeout = iterations;
+}
+
+void microbit_panic( int statusCode)
+{
+    const microbit_LEDMapStr &mm = microbit_LEDMap;
+    uint8_t chr0 = 255;
+    uint8_t chr;
+    
+    target_disable_irq();
+    
+    microbit_LEDMap_configure();
+    
+    for ( int repeat = 0; panic_timeout == 0 || repeat < panic_timeout; repeat++)
+    {
+        for ( int msgIdx = 0; msgIdx < microbit_panic_MSGLEN; msgIdx++, chr0 = chr)
+        {
+            // find the the current character and its font bytes
+            chr = msgIdx ? 0x45 : 0;
+            if ( msgIdx >= microbit_panic_MSG1STDIGIT)
+            {
+                // calculate divisor for this digit: 100s, 10s or units
+                int div = microbit_panic_DIVMAX;
+                for ( int digit = msgIdx - microbit_panic_MSG1STDIGIT; digit > 0; digit--)
+                    div /= microbit_panic_DIVBASE;
+                chr = 0x30 + ( statusCode / div) % microbit_panic_DIVBASE;
+            }
+            const uint8_t *fontBytes = chr ? ( BitmapFont::defaultFont + BITMAP_FONT_WIDTH * ( chr - BITMAP_FONT_ASCII_START)) : panic_face;
+            
+            for ( int scan = 0; scan < microbit_panic_SCANS; scan++)
+            {
+                int rowOnCycles = microbit_panic_ROWDELAY;
+                if ( chr == chr0 && scan < microbit_panic_CLEARSCANS)   // blank display for repeated characters
+                    rowOnCycles = 0;
+                else if ( repeat >= 5)                                  // dim display after 5 repetitions
+                    rowOnCycles = rowOnCycles / 10;
+                
+                // turn each row on and off
+                for ( int row = 0; row < mm.rows; row++)
+                {
+                    for ( int col = 0; col < mm.columns; col++)
+                    {
+                        microbit_LEDMap_columnOn( col,
+                            fontBytes[ mm.mapRCtoY( row, col)] & ( 1 << ( mm.width - 1 - mm.mapRCtoX( row, col))));
+                    }
+                        
+                    if ( rowOnCycles)
+                      microbit_LEDMap_rowOn( row);
+
+                    for ( int i = 0; i < microbit_panic_ROWDELAY; i++)
+                    {
+                        nrf_gpio_pin_out_read( microbit_LEDMap.columnPins[0]);
+                        if ( i == rowOnCycles - 1)
+                            microbit_LEDMap_rowOff( row);
+                    }
+                }
+            }
+        }
+    }
+    
+    microbit_reset();
+}
+
+} // namespace codal
+
+
+// Override for CODAL target HAL
+
+void target_panic( int statusCode)
+{
+    codal::microbit_panic( statusCode);
 }
