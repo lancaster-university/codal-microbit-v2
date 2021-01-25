7bf3cce03c366b93790fdad2cd67c48a5518842a
Joshua Hill <j.hill8@lancaster.ac.uk>
2021-01-25 09:53:20 +0000

    Updating fft branch in line with origin. Added ability to query MicroBitAudioProcessor and sample fft C++ Program

---
+++ samples/fft_example.cpp
@@ -0,0 +1,76 @@
+#include "MicroBit.h"
+#include "CodalDmesg.h"
+#include "MicroBitAudioProcessor.h"
+#include "StreamNormalizer.h"
+#include "Tests.h"
+
+static NRF52ADCChannel *mic = NULL;
+static StreamNormalizer *processor = NULL;
+static MicroBitAudioProcessor *fft = NULL;
+
+MicroBit uBit;
+
+/**
+  * fft_test function - creates an example MicroBitAudioProcessor and then queries it for results.
+  * Currently configured to use 1024 samples with 8bit signed data.
+  */
+void fft_test(){
+    uBit.display.print("L");
+     if (mic == NULL){
+        mic = uBit.adc.getChannel(uBit.io.microphone);
+        mic->setGain(7,0);
+    }
+
+    if (processor == NULL)
+        processor = new StreamNormalizer(mic->output, 1.0f, true, DATASTREAM_FORMAT_8BIT_SIGNED, 10);
+
+    if (fft == NULL)
+        fft = new MicroBitAudioProcessor(processor->output);
+
+    uBit.io.runmic.setDigitalValue(1);
+    uBit.io.runmic.setHighDrive(true);
+
+
+    //Start fft running
+    fft->startRecording();
+
+
+    while(1){
+        //TODO - de-noise : if last X samples are same - display ect.
+        //The output values depend on the input type (DATASTREAM_FORMAT_8BIT_SIGNED) and the size
+        //of the FFT - which is changed using the 'AUDIO_SAMPLES_NUMBER' in MicroBitAudioProcessor.h
+        //default is 1024
+        uBit.sleep(100);
+        int freq = fft->getFrequency();
+        DMESG("%s %d", "frequency: ", freq);
+        if(freq > 0)
+            uBit.display.print("?");
+        if(freq > 530)
+            uBit.display.print("C");
+        if(freq > 600)
+            uBit.display.print("D");
+        if(freq > 680)
+            uBit.display.print("E");
+        if(freq > 710)
+            uBit.display.print("F");
+        if(freq > 800)
+            uBit.display.print("G");
+        if(freq > 900)
+            uBit.display.print("A");
+        if(freq > 1010)
+            uBit.display.print("B");
+        if(freq > 1050)
+            uBit.display.print("?");
+    }
+}
+
+
+
+int 
+main()
+{
+
+    uBit.init();
+    fft_test();
+    
+}
