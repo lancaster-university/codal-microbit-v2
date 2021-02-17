2091d4e86eacda5afa4e0ab48a4f2ca4c8d42daa
Sam Kent <sam@microbit.org>
2021-02-17 17:07:06 +0000

    fixup! Use region ids for MakeCode too

--- inc/bluetooth/MicroBitPartialFlashingService.h
+++ inc/bluetooth/MicroBitPartialFlashingService.h
@@ -94,11 +94,6 @@ class MicroBitPartialFlashingService : public MicroBitBLEService
      */
     void noValidation();
 
-    /**
-     * Find and erase Micropython Filesystem
-     */
-    void findAndEraseMicroPythonFS();
-
     // Ensure packets are in order
     uint8_t packetCount = 0;
     uint8_t blockPacketCount = 0;
