7415cf37ecf80a9c0edc8504771152c5824da5aa
Vlad Turcuman <vladturcuman@gmail.com>
2021-05-01 18:40:31 +0100

    Added comments to the sound recogniser

--- inc/MicroBitSoundRecogniser.h
+++ inc/MicroBitSoundRecogniser.h
@@ -7,84 +7,222 @@
 #include "MicroBitAudioProcessor.h"
 #include "arm_math.h"
 
-// Should be the minimum number of elements needed in the buffer 
-// at any time
-#define HISTORY_LENGTH 50
+/*
+ *
+ * The sound recogniser takes in data from the audio processor - in
+ * form of AudioFrameAnalysis. It then tries to match the history 
+ * against samples of sounds. 
+ *  
+ *  
+ * Sound fingerprint
+ *  
+ * A sound has multiple sequences, each sequence having multiple samples
+ * to account for the randomness. 
+ *  
+ * Each sequence also has:
+ *     a threshold -   the maximum absolute difference from the sampled 
+ *                     frequency and the heard frequency.  
+ *     maximum number of deviations - the maximum number of datapoints 
+ *                     that can be more than the threshold away from the
+ *                     sampled frequency
+ *  
+ * A sound also has a maximum number of deviations - the total maximum 
+ * deviations across all sequences.
+ */
+
+
+/*
+ * The maximum length of the buffer.
+ * 
+ * It should be >= than the maximum length of datapoints in 
+ * any sample.
+ */
+#define HISTORY_LENGTH 30
 
 class MicroBitSoundRecogniser : public DataSink
 {   
     private:
-        MicroBitAudioProcessor& audio_proceesor;
+
+        MicroBitAudioProcessor& audio_proceesor;    // the stream of data from the audio processor to analyse 
         
-        bool analysing;
+        bool analysing;                             // whether it should analyse the data or be idle
 
-        void (*callback)(ManagedString) = NULL;
+        void (*callback)(ManagedString) = NULL;     // the callback function when a sound is detected
 
-        MicroBitAudioProcessor::AudioFrameAnalysis buffer[2 * HISTORY_LENGTH];
-        uint8_t buffer_len;
+        MicroBitAudioProcessor::AudioFrameAnalysis buffer[2 * HISTORY_LENGTH];  // the buffer to collect the incoming data in
+        uint8_t buffer_len;                                                     // the length of the buffer
 
     protected:
+
+        /*
+         * A struct to package a sample.
+         */
         struct SoundSample {
             SoundSample(const uint16_t* _frames, uint8_t size);
             ~SoundSample();
 
-            uint8_t     size;
-            uint16_t*   frames;
+            uint8_t         size;   // the number of data points in the sample
+            const uint16_t* frames; // the data points 
         };
 
+
+        /*
+         * A struct to package a sequence.
+         */
         struct SoundSequence {
             SoundSequence(uint8_t size, uint32_t threshold, uint8_t deviation);
             ~SoundSequence();
 
-            uint8_t         size;
-            uint32_t        threshold;
-            uint8_t         deviation;
-            SoundSample**   samples;
+            uint8_t         size;       // the number of samples
+            uint32_t        threshold;  // the threshold for the sequence
+            uint8_t         deviation;  // the maximum number of deviations allowed for the sequence
+            SoundSample**   samples;    // ponter to the array of samples
         };
 
+
+        /*
+         * A struct to package a sound.
+         */
         struct Sound {
             Sound(uint8_t size, uint8_t max_deviation, uint8_t max_history_len, bool consider_all_frequencies);
             ~Sound();
             
-            bool            consider_all_frequencies;
-            uint8_t         max_deviation;
-            uint8_t         size;
-            SoundSequence** sequences;
+            bool            consider_all_frequencies;   // whether or not to consider all frequencies detected or just the dominant one
+            uint8_t         max_deviation;              // the maximum total number of deviations allowed
+            uint8_t         size;                       // the number of sequences in the sound
+            SoundSequence** sequences;                  // pointer to the array of sequences
             
-            void    update( MicroBitAudioProcessor::AudioFrameAnalysis* buffer, 
-                            uint8_t buffer_len );
-            bool    matched();
-            void    resetHistory();
+            /*
+             * Update called when new data comes in.
+             *
+             * @param buffer the buffer with the last data points that came in
+             * 
+             * @note This keeps the history updated which is needed for the 
+             *       dynamic programming approach chosen for matching
+             */
+            void update( MicroBitAudioProcessor::AudioFrameAnalysis* buffer, 
+                         uint8_t buffer_len );
+
+            /*
+             * Whether or not the sound matched in the last frame.
+             *
+             * @return whether or not the sound matched in the last frame.
+             * 
+             * @note Should be called after update when new data comes in.
+             */
+            bool matched();
+
+            /*
+             * Resets the history buffer.
+             *
+             * @note Used when the data stops coming in - e.g. when the analyser 
+             *       is paused
+             */
+            void resetHistory();
 
             private:
             
+            /* 
+             * Matches a sequence to the last couple of data points in the buffer.
+             * 
+             * @param seq_id the id of the sequence to try to match
+             * 
+             * @param buffer the buffer of data points that came in
+             * 
+             * @param buffer_len the length of the buffer
+             * 
+             * @return the number of deviations in the last data points to the sound up to 
+             *         the seq_id sequence or 255 if it's above the maximums allowed.
+             */
             uint8_t matchSequence(  uint8_t seq_id, 
                                     MicroBitAudioProcessor::AudioFrameAnalysis* buffer, 
                                     uint8_t buffer_len) const;
 
+            /* 
+             * Getter for the internal history buffer of the sound.
+             *
+             * @param frames_ago the number of frames ago for which the
+             *        query was made
+             * 
+             * @param seq_id the id of the sequence to get the deviation for
+             * 
+             * @return the deviation (or 255 if it didn't match) up to the 
+             *         sequence seq_id that was frames_ago frames ago.
+             * 
+             * @note used to check if the sound matched up to the first seq_id
+             *       sequences so that the matching doesn't need to recheck 
+             *       those sequences.
+             */
             uint8_t getDeviation(uint8_t frames_ago, uint8_t seq_id) const;
+
+            /*
+             * Adds to the history buffer a deviation for a certain sequence.
+             *
+             * @param seq_id the id of the sequence to add the value to.
+             * 
+             * @param value the value to be added to the history buffer
+             */
             void    addToHistory(uint8_t seq_id, uint8_t value);
+
+            /*
+             * Ends a history frame, increasing the length of the buffer. 
+             */
             void    endHistoryFrame();
 
-            uint8_t*    history;
-            uint8_t     history_len;
-            uint8_t     max_history_len;
+            uint8_t*    history;            // the array of to keep the history buffer in
+            uint8_t     history_len;        // the size of the history buffer
+            uint8_t     max_history_len;    // the maximum length of the history buffer. Used for double buffering
         };
 
+        /*
+         * Constructor.
+         * 
+         * Initialize the MicroBitSoundRecogniser.
+         * 
+         * @note is private to make the class abstract.
+         */
         MicroBitSoundRecogniser(MicroBitAudioProcessor& processor);
         
-        Sound**         sounds;
-        ManagedString** sounds_names;
-        uint8_t         sounds_size;
+        Sound**         sounds;         // pointer to the array of sounds to recognise
+        ManagedString** sounds_names;   // pointer to the array of sound names
+        uint8_t         sounds_size;    // the number of sounds to try to recognise
 
     public:
+
+        /*
+         * Destructor.
+         * 
+         * Deallocates all the memory allocated dynamically.
+         */
         ~MicroBitSoundRecogniser();
 
+
+        /*
+         * A callback for when the data is ready.
+         */
         virtual int pullRequest();
         
-        MicroBitAudioProcessor* getAudioProcessor();
+        /*
+         * Sets the callback. 
+         * 
+         * @TODO change it to send a message on the message bus 
+         *       rather than having a callback
+         */
         void setCallback(void (*_callback)(ManagedString));
+
+        /*
+         * Starts analysing the data that comes in. Also sets the callback. 
+         * 
+         * @TODO change it to send a message on the message bus 
+         *       rather than having a callback
+         */
         void startAnalisying(void (*_callback)(ManagedString));
+
+        
+        /*
+         * Stops analysing the data and also stops the audio processor
+         * from receiving.
+         */
         void stopAnalisying();
 };
 
--- source/MicroBitSoundRecogniser.cpp
+++ source/MicroBitSoundRecogniser.cpp
@@ -1,6 +1,11 @@
 
 #include "MicroBitSoundRecogniser.h"
 
+/*
+ * Constructor.
+ * 
+ * Initialize the MicroBitSoundRecogniser.
+ */
 MicroBitSoundRecogniser::MicroBitSoundRecogniser(MicroBitAudioProcessor& audio_processor) 
                                 : audio_proceesor(audio_processor){
     analysing = false;
@@ -9,6 +14,11 @@ MicroBitSoundRecogniser::MicroBitSoundRecogniser(MicroBitAudioProcessor& audio_p
     sounds_size = 0;
 }
 
+/*
+ * Destructor.
+ * 
+ * Deallocates all the memory allocated dynamically.
+ */
 MicroBitSoundRecogniser::~MicroBitSoundRecogniser(){
     if(sounds_size != 0){
         for(uint8_t i = 0; i < sounds_size; i++) {
@@ -20,24 +30,29 @@ MicroBitSoundRecogniser::~MicroBitSoundRecogniser(){
     }
 }
 
+
+/*
+ * A callback for when the data is ready.
+ */
 int MicroBitSoundRecogniser::pullRequest(){
 
     auto frames = audio_proceesor.pull();
 
     if(!analysing) return DEVICE_OK;
 
-    // I only send one at a time when calling pullRequest from MicroBitAudioProcessor.
-    // Is there a way to make them concurrent -- might save some resources?
     MicroBitAudioProcessor::AudioFrameAnalysis* buf = (MicroBitAudioProcessor::AudioFrameAnalysis* ) &frames[0];
     buffer[buffer_len].size = buf[0].size;
     for(uint8_t i = 0; i<buf[0].size; i++)  
         buffer[buffer_len].buf[i] = buf[0].buf[i];
     buffer_len ++;
 
-    // gives an amortised O(1) for keeping a buffer in order. 
-    // could be changed to a circular buffer to reduce the memory
-    // to 1/2 if needed -- more complex functions for accessing it 
-    // would be necessary which might have a greater constant than this 
+    /* Double buffering
+     *
+     * gives an amortised O(1) for keeping a buffer in order. 
+     * could be changed to a circular buffer to reduce the memory
+     * to 1/2 if needed -- more complex functions for accessing it 
+     * would be necessary which might have a greater impact than this
+     */
     if(buffer_len == 2 * HISTORY_LENGTH){ 
         memcpy(&buffer[0], &buffer[HISTORY_LENGTH], 
                         sizeof(MicroBitAudioProcessor::AudioFrameAnalysis) * HISTORY_LENGTH);
@@ -55,21 +70,33 @@ int MicroBitSoundRecogniser::pullRequest(){
 
     return DEVICE_OK;
 }
-
-MicroBitAudioProcessor* MicroBitSoundRecogniser::getAudioProcessor(){
-    return &audio_proceesor;
-}
-
+        
+/*
+ * Sets the callback. 
+ * 
+ * @TODO change it to send a message on the message bus 
+ *       rather than having a callback
+ */
 void MicroBitSoundRecogniser::setCallback(void (*_callback)(ManagedString)){
     callback = _callback;
 }
 
+/*
+ * Starts analysing the data that comes in. Also sets the callback. 
+ * 
+ * @TODO change it to send a message on the message bus 
+ *       rather than having a callback
+ */
 void MicroBitSoundRecogniser::startAnalisying(void (*_callback)(ManagedString)){
     setCallback(_callback);
     analysing = true;
     audio_proceesor.startRecording();
 }
 
+/*
+ * Stops analysing the data and also stops the audio processor
+ * from receiving.
+ */
 void MicroBitSoundRecogniser::stopAnalisying(){
     analysing = false;
     buffer_len = 0;
@@ -78,11 +105,9 @@ void MicroBitSoundRecogniser::stopAnalisying(){
         sounds[sound_it] -> resetHistory();
 }
 
+
 MicroBitSoundRecogniser::SoundSample::SoundSample(const uint16_t* _frames, uint8_t size) 
-                                    : size(size) {
-    frames = new uint16_t[size];
-    memcpy(frames, _frames, sizeof(uint16_t) * size);
-}
+                                    : size(size), frames(_frames) { }
 
 MicroBitSoundRecogniser::SoundSample::~SoundSample() { 
     delete[] frames; 
@@ -116,6 +141,14 @@ MicroBitSoundRecogniser::Sound::~Sound() {
     delete [] history;
 }
 
+/*
+ * Update called when new data comes in.
+ *
+ * @param buffer the buffer with the last data points that came in
+ * 
+ * @note This keeps the history updated which is needed for the 
+ *       dynamic programming approach chosen for matching
+ */
 void MicroBitSoundRecogniser::Sound::update(MicroBitAudioProcessor::AudioFrameAnalysis* buffer, 
                                     uint8_t buffer_len){
     for(uint8_t seq_it = 0; seq_it < size; seq_it ++) {
@@ -125,6 +158,13 @@ void MicroBitSoundRecogniser::Sound::update(MicroBitAudioProcessor::AudioFrameAn
     endHistoryFrame();
 }
 
+/*
+ * Whether or not the sound matched in the last frame.
+ *
+ * @return whether or not the sound matched in the last frame.
+ * 
+ * @note Should be called after update when new data comes in.
+ */
 bool MicroBitSoundRecogniser::Sound::matched() {
     if(getDeviation(1, size - 1) <= max_deviation){
         history_len = 0;
@@ -132,7 +172,19 @@ bool MicroBitSoundRecogniser::Sound::matched() {
     }
     return false;
 }
-
+            
+/* 
+ * Matches a sequence to the last couple of data points in the buffer.
+ * 
+ * @param seq_id the id of the sequence to try to match
+ * 
+ * @param buffer the buffer of data points that came in
+ * 
+ * @param buffer_len the length of the buffer
+ * 
+ * @return the number of deviations in the last data points to the sound up to 
+ *         the seq_id sequence or 255 if it's above the maximums allowed.
+ */
 uint8_t MicroBitSoundRecogniser::Sound::matchSequence(uint8_t seq_id,
                                             MicroBitAudioProcessor::AudioFrameAnalysis* buffer, 
                                             uint8_t buffer_len) const {
@@ -188,25 +240,55 @@ uint8_t MicroBitSoundRecogniser::Sound::matchSequence(uint8_t seq_id,
     return min_dev;           
 }
 
-
+/* 
+ * Getter for the internal history buffer of the sound.
+ *
+ * @param frames_ago the number of frames ago for which the
+ *        query was made
+ * 
+ * @param seq_id the id of the sequence to get the deviation for
+ * 
+ * @return the deviation (or 255 if it didn't match) up to the 
+ *         sequence seq_id that was frames_ago frames ago.
+ * 
+ * @note used to check if the sound matched up to the first seq_id
+ *       sequences so that the matching doesn't need to recheck 
+ *       those sequences.
+ */
 uint8_t MicroBitSoundRecogniser::Sound::getDeviation(uint8_t frames_ago, uint8_t seq_id) const {
     if(history_len < frames_ago) return 255;
     return history[(history_len - frames_ago) * size + seq_id];
 }
 
+/*
+ * Adds to the history buffer a deviation for a certain sequence.
+ *
+ * @param seq_id the id of the sequence to add the value to.
+ * 
+ * @param value the value to be added to the history buffer
+ */
 void MicroBitSoundRecogniser::Sound::addToHistory(uint8_t seq_id, uint8_t value){
     history[history_len * size + seq_id] = value;
 }
 
+/*
+ * Ends a history frame, increasing the length of the buffer. 
+ */
 void MicroBitSoundRecogniser::Sound::endHistoryFrame(){
     history_len ++;
-    // same type of buffer as the buffer from MicroBitSoundRecogniser
+    // double buffering
     if(history_len == 2 * max_history_len) {
         memcpy(&history[0], &history[max_history_len * size], sizeof(uint8_t) * max_history_len * size);
         history_len = max_history_len;
     }
 }
 
+/*
+ * Resets the history buffer.
+ *
+ * @note Used when the data stops coming in - e.g. when the analyser 
+ *       is paused
+ */
 void MicroBitSoundRecogniser::Sound::resetHistory(){
     history_len = 0;
 }
