ed5552107b4e9b541eff602c880b197dbf4b8658
danielstl <15856061+danielstl@users.noreply.github.com>
2022-08-10 19:11:32 +0100

    Clarified log full and created log empty message for offline editor, with further minifications to support this

--- resources/logfs/dl.js
+++ resources/logfs/dl.js
@@ -83,10 +83,11 @@ function UserGraphError(message) {
           document.head.appendChild(graphLib);
         }
 
-        base.load();
+        // base.load() returns the raw data contained after FS_START, meaning we
+        // can parse it again here to fetch the DAPLink version - saves bytes!
+        const rawData = base.load();
 
-        // The base.load() function attaches the DAPLink version to the original object
-        this.daplinkVersion = base.dapVer;
+        this.daplinkVersion = parseInt(rawData.substring(40, 44));
       },
 
       hasGraph: function () {
