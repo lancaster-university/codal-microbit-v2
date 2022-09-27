2ab56d1cf5b6260676257f16e980e0580e2dd8de
Joe Finney <joe@comp.lancs.ac.uk>
2022-09-27 18:46:58 +0100

    Introduce egress double buffering onto SoundEmojiSynth
      - Add double buffer
      - Introduce support for partial filling of DMA buffering, with later completion.

--- source/SoundEmojiSynthesizer.cpp
+++ source/SoundEmojiSynthesizer.cpp
@@ -41,6 +41,8 @@ SoundEmojiSynthesizer::SoundEmojiSynthesizer(uint16_t id, int sampleRate) : Coda
     this->bufferSize = EMOJI_SYNTHESIZER_BUFFER_SIZE;
     this->position = 0.0f;
     this->effect = NULL;
+    this->partialBuffer = NULL;
+    this->buffer2 = ManagedBuffer(bufferSize);
 
     this->samplesToWrite = 0;
     this->samplesWritten = 0;
@@ -162,7 +164,6 @@ bool SoundEmojiSynthesizer::nextSoundEffect()
             effectBuffer = emptyBuffer;
             samplesWritten = 0;
             samplesToWrite = 0;
-            position = 0.0f;
             return hadEffect;
         }
     }
@@ -188,6 +189,30 @@ bool SoundEmojiSynthesizer::nextSoundEffect()
  */
 ManagedBuffer SoundEmojiSynthesizer::pull()
 {
+    ManagedBuffer output = buffer2;
+
+    // If the last DMA buffer was only partially filled, try to fill it.
+    if (partialBuffer)
+        fillOutputBuffer();
+
+    // We only allow one chance at filling a DMA buffer, so that we don't stall the pipeline.
+    partialBuffer = NULL;
+    buffer2 = buffer;
+
+    // Generate another buffer if possible. This may be empty if there is no sound effect scheduled.
+    fillOutputBuffer();
+
+    // Issue a Pull Request so that we are always receiver driven, and we're done.
+    downStream->pullRequest();
+
+    return output;
+}
+
+/**
+ * Provide the next available ManagedBuffer to our downstream caller, if available.
+ */
+ManagedBuffer SoundEmojiSynthesizer::fillOutputBuffer()
+{
     // Generate a buffer on demand. This is likely to be in interrupt context, so
     // the receiver driven nature reduces glitching on audio output.
     bool done = false;
@@ -207,6 +232,9 @@ ManagedBuffer SoundEmojiSynthesizer::pull()
                 done = true;
                 if (renderComplete || status & EMOJI_SYNTHESIZER_STATUS_STOPPING)
                 {
+                    if (renderComplete)
+                        partialBuffer = sample;
+
                     // Flip our status bit and fire the event
                     status &= ~EMOJI_SYNTHESIZER_STATUS_STOPPING;
                     Event(id, DEVICE_SOUND_EMOJI_SYNTHESIZER_EVT_DONE);
@@ -219,8 +247,18 @@ ManagedBuffer SoundEmojiSynthesizer::pull()
         // We defer creation to avoid unecessary heap allocation when genertaing silence.
         if (((samplesWritten < samplesToWrite) || !(status & EMOJI_SYNTHESIZER_STATUS_OUTPUT_SILENCE_AS_EMPTY)) && sample == NULL)
         {
-            buffer = ManagedBuffer(bufferSize);
-            sample = (uint16_t *) &buffer[0];
+            // If we completed the last sound effect in the middl eof a DMA buffer, continue from where we left off
+            if (partialBuffer)
+            {
+                sample = partialBuffer;
+                partialBuffer = NULL;
+            }
+            else
+            {
+                buffer = ManagedBuffer(bufferSize);
+                sample = (uint16_t *) &buffer[0];
+            }
+
             bufferEnd = (uint16_t *) (&buffer[0] + buffer.length());
         }
 
@@ -249,10 +287,7 @@ ManagedBuffer SoundEmojiSynthesizer::pull()
             {
                 // Stop processing when we've filled the requested buffer
                 if (sample == bufferEnd)
-                {
-                    downStream->pullRequest();
                     return buffer;
-                }
 
                 // Synthesize a sample
                 float s = effect->tone.tonePrint(effect->tone.parameter, (int) position);
@@ -303,8 +338,6 @@ ManagedBuffer SoundEmojiSynthesizer::pull()
         }
     }
 
-    // Issue a Pull Request so that we are always receiver driven, and we're done.
-    downStream->pullRequest();
     return buffer;
 }
 
