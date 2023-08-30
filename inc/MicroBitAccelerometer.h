d0831432c5d67838afca200b2c07a4560406f64c
Carlos Pereira Atencio <carlos@microbit.org>
2023-08-30 13:23:00 +0100

    Fix issue when missing motion sensors crashed instead of panicking. (#360)
    MicroBitAccelerometer & MicroBitCompass instances were never created, so the methods that checked for a null pointer to throw the panic weren't being executed.

--- inc/MicroBitAccelerometer.h
+++ inc/MicroBitAccelerometer.h
@@ -27,84 +27,53 @@ DEALINGS IN THE SOFTWARE.
 
 #include "CodalConfig.h"
 #include "MicroBitCompat.h"
-#include "codal-core/inc/core/CodalComponent.h"
 #include "codal-core/inc/driver-models/Accelerometer.h"
-#include "codal-core/inc/driver-models/Compass.h"
-#include "codal-core/inc/driver-models/I2C.h"
-#include "codal-core/inc/driver-models/Pin.h"
 #include "codal-core/inc/types/CoordinateSystem.h"
+#include "MicroBitI2C.h"
 
 
-/**
- * Class definition for MicroBitAccelerometer.
- */
-class MicroBitAccelerometer : public Accelerometer
+namespace codal
 {
-    public:
+    /**
+     * Class definition for MicroBitAccelerometer.
+     */
+    class MicroBitAccelerometer : public Accelerometer
+    {
+      public:
 
         static Accelerometer* driver;                     // The instance of an Accelerometer driver.
-        static NRF52Pin irq1;                             // IRQ pin for detected acceleromters to use
-        static CoordinateSpace coordinateSpace;           // Default coordinate space
 
         /**
          * Constructor.
-         * Create a software abstraction of an accelerometer.
+         * Create an instance of an accelerometer that detects which on-board
+         * sensor is present and initialises the respective driver.
          *
-         * @param coordinateSpace the orientation of the sensor. Defaults to: SIMPLE_CARTESIAN
+         * @param coordinateSpace the orientation of the sensor.
          * @param id the unique EventModel id of this component. Defaults to: MICROBIT_ID_ACCELEROMETER
          *
          */
-        MicroBitAccelerometer(MicroBitI2C &i2c, uint16_t id = MICROBIT_ID_ACCELEROMETER);
+        MicroBitAccelerometer(MicroBitI2C &i2c, CoordinateSpace &coordinateSpace, uint16_t id = MICROBIT_ID_ACCELEROMETER);
+
+        /**
+         * Constructor for an empty driver.
+         *
+         * This constructor should only be called when the on-board sensor is not
+         * detected, and we need an instance that's able to panic on first usage.
+         *
+         * @param coordinateSpace the orientation of the sensor.
+         * @param id the unique EventModel id of this component. Defaults to: MICROBIT_ID_ACCELEROMETER
+         *
+         */
+        MicroBitAccelerometer(CoordinateSpace &coordinateSpace, uint16_t id = MICROBIT_ID_ACCELEROMETER);
 
         /**
          * Device autodetection. Scans the given I2C bus for supported accelerometer devices.
          * if found, constructs an appropriate driver and returns it.
          *
-         * @param i2c the bus to scan. 
+         * @param i2c the bus to scan.
          *
          */
         static Accelerometer& autoDetect(MicroBitI2C &i2c); 
-
-        /**
-         * Attempts to set the sample rate of the accelerometer to the specified value (in ms).
-         *
-         * @param period the requested time between samples, in milliseconds.
-         * @return MICROBIT_OK on success, MICROBIT_I2C_ERROR is the request fails.
-         *
-         * @note The requested rate may not be possible on the hardware. In this case, the
-         * nearest lower rate is chosen.
-         *
-         * @note This method should be overriden (if supported) by specific accelerometer device drivers.
-         */
-        int setPeriod(int period);
-
-        /**
-         * Reads the currently configured sample rate of the accelerometer.
-         *
-         * @return The time between samples, in milliseconds.
-         */
-        int getPeriod();
-
-        /**
-         * Attempts to set the sample range of the accelerometer to the specified value (in g).
-         *
-         * @param range The requested sample range of samples, in g.
-         *
-         * @return MICROBIT_OK on success, MICROBIT_I2C_ERROR is the request fails.
-         *
-         * @note The requested range may not be possible on the hardware. In this case, the
-         * nearest lower range is chosen.
-         *
-         * @note This method should be overriden (if supported) by specific accelerometer device drivers.
-         */
-        int setRange(int range);
-
-        /**
-         * Reads the currently configured sample range of the accelerometer.
-         *
-         * @return The sample range, in g.
-         */
-        int getRange();
 
         /**
          * Configures the accelerometer for G range and sample rate defined
@@ -113,125 +82,24 @@ class MicroBitAccelerometer : public Accelerometer
          * updated to reflect reality.
          *
          * @return MICROBIT_OK on success, MICROBIT_I2C_ERROR if the accelerometer could not be configured.
-         *
-         * @note This method should be overidden by the hardware driver to implement the requested
-         * changes in hardware.
          */
-        int configure();
+        virtual int configure() override;
 
         /**
          * Poll to see if new data is available from the hardware. If so, update it.
-         * n.b. it is not necessary to explicitly call this funciton to update data
+         * n.b. it is not necessary to explicitly call this function to update data
          * (it normally happens in the background when the scheduler is idle), but a check is performed
          * if the user explicitly requests up to date data.
          *
          * @return MICROBIT_OK on success, MICROBIT_I2C_ERROR if the update fails.
-         *
-         * @note This method should be overidden by the hardware driver to implement the requested
-         * changes in hardware.
          */
-        int requestUpdate();
-
-        /**
-         * Reads the last accelerometer value stored, and provides it in the coordinate system requested.
-         *
-         * @param coordinateSpace The coordinate system to use.
-         * @return The force measured in each axis, in milli-g.
-         */
-        Sample3D getSample(CoordinateSystem coordinateSystem);
-
-        /**
-         * Reads the last accelerometer value stored, and in the coordinate system defined in the constructor.
-         * @return The force measured in each axis, in milli-g.
-         */
-        Sample3D getSample();
-
-        /**
-         * reads the value of the x axis from the latest update retrieved from the accelerometer,
-         * using the default coordinate system as specified in the constructor.
-         *
-         * @return the force measured in the x axis, in milli-g.
-         */
-        int getX();
-
-        /**
-         * reads the value of the y axis from the latest update retrieved from the accelerometer,
-         * using the default coordinate system as specified in the constructor.
-         *
-         * @return the force measured in the y axis, in milli-g.
-         */
-        int getY();
-
-        /**
-         * reads the value of the z axis from the latest update retrieved from the accelerometer,
-         * using the default coordinate system as specified in the constructor.
-         *
-         * @return the force measured in the z axis, in milli-g.
-         */
-        int getZ();
-
-        /**
-         * Provides a rotation compensated pitch of the device, based on the latest update retrieved from the accelerometer.
-         *
-         * @return The pitch of the device, in degrees.
-         *
-         * @code
-         * accelerometer.getPitch();
-         * @endcode
-         */
-        int getPitch();
-
-        /**
-         * Provides a rotation compensated pitch of the device, based on the latest update retrieved from the accelerometer.
-         *
-         * @return The pitch of the device, in radians.
-         *
-         * @code
-         * accelerometer.getPitchRadians();
-         * @endcode
-         */
-        float getPitchRadians();
-
-        /**
-         * Provides a rotation compensated roll of the device, based on the latest update retrieved from the accelerometer.
-         *
-         * @return The roll of the device, in degrees.
-         *
-         * @code
-         * accelerometer.getRoll();
-         * @endcode
-         */
-        int getRoll();
-
-        /**
-         * Provides a rotation compensated roll of the device, based on the latest update retrieved from the accelerometer.
-         *
-         * @return The roll of the device, in radians.
-         *
-         * @code
-         * accelerometer.getRollRadians();
-         * @endcode
-         */
-        float getRollRadians();
-
-        /**
-         * Retrieves the last recorded gesture.
-         *
-         * @return The last gesture that was detected.
-         *
-         * Example:
-         * @code
-         *
-         * if (accelerometer.getGesture() == SHAKE)
-         *     display.scroll("SHAKE!");
-         * @endcode
-         */
-        uint16_t getGesture();
+        virtual int requestUpdate() override;
 
         /**
          * Destructor.
          */
         ~MicroBitAccelerometer();
-};
+    };
+}
 
 #endif
--- source/MicroBitCompass.cpp
+++ source/MicroBitCompass.cpp
@@ -31,320 +31,55 @@ DEALINGS IN THE SOFTWARE.
 #include "MicroBitError.h"
 #include "LSM303Magnetometer.h"
 
-Compass* MicroBitCompass::driver;
+using namespace codal;
 
-/**
- * Constructor.
- * Create a software abstraction of an e-compass.
- *
- * @param id the unique EventModel id of this component. Defaults to: MICROBIT_ID_COMPASS
- * @param coordinateSpace the orientation of the sensor. Defaults to: SIMPLE_CARTESIAN
- *
- */
-MicroBitCompass::MicroBitCompass(MicroBitI2C &i2c, uint16_t id) : Compass(MicroBitAccelerometer::coordinateSpace)
+Compass* MicroBitCompass::driver = NULL;
+
+MicroBitCompass::MicroBitCompass(MicroBitI2C &i2c, CoordinateSpace &coordinateSpace, uint16_t id) : Compass(coordinateSpace, id)
 {
     autoDetect(i2c);
 }
 
-/**
- * Device autodetection. Scans the given I2C bus for supported accelerometer devices.
- * if found, constructs an appropriate driver and returns it.
- *
- * @param i2c the bus to scan. 
- * @param id the unique EventModel id of this component. Defaults to: MICROBIT_ID_ACCELEROMETER
- *
- */
+MicroBitCompass::MicroBitCompass(CoordinateSpace &coordinateSpace, uint16_t id) : Compass(coordinateSpace, id)
+{
+}
+
 Compass& MicroBitCompass::autoDetect(MicroBitI2C &i2c)
 {
-    /*
-     * In essence, the LSM needs at least 6.4ms from power-up before we can use it.
-     * https://github.com/microbit-foundation/codal-microbit/issues/33
-     */
-    target_wait(10);
-
-    // We only have combined sensors, so rely on the accelerometer detection code to also detect the magnetomter.
+    // We only have combined sensors, so rely on the accelerometer detection code to also detect the magnetometer.
     MicroBitAccelerometer::autoDetect(i2c);
 
     return *MicroBitCompass::driver;
 }
 
-/**
- * Gets the current heading of the device, relative to magnetic north.
- *
- * If the compass is not calibrated, it will raise the MICROBIT_COMPASS_EVT_CALIBRATE event.
- *
- * Users wishing to implement their own calibration algorithms should listen for this event,
- * using MESSAGE_BUS_LISTENER_IMMEDIATE model. This ensures that calibration is complete before
- * the user program continues.
- *
- * @return the current heading, in degrees. Or CALIBRATION_IN_PROGRESS if the compass is calibrating.
- *
- * @code
- * compass.heading();
- * @endcode
- */
-int MicroBitCompass::heading()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-
-    return driver->heading();
-}
-
-/**
- * Determines the overall magnetic field strength based on the latest update from the magnetometer.
- *
- * @return The magnetic force measured across all axis, in nano teslas.
- *
- * @code
- * compass.getFieldStrength();
- * @endcode
- */
-int MicroBitCompass::getFieldStrength()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->getFieldStrength();
-}
-
-/**
- * Perform a calibration of the compass.
- *
- * This method will be called automatically if a user attempts to read a compass value when
- * the compass is uncalibrated. It can also be called at any time by the user.
- *
- * The method will only return once the compass has been calibrated.
- *
- * @return MICROBIT_OK, MICROBIT_I2C_ERROR if the magnetometer could not be accessed,
- * or MICROBIT_CALIBRATION_REQUIRED if the calibration algorithm failed to complete successfully.
- *
- * @note THIS MUST BE CALLED TO GAIN RELIABLE VALUES FROM THE COMPASS
- */
-int MicroBitCompass::calibrate()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->calibrate();
-}
-
-/**
- * Configure the compass to use the calibration data that is supplied to this call.
- *
- * Calibration data is comprised of the perceived zero offset of each axis of the compass.
- *
- * After calibration this should now take into account trimming errors in the magnetometer,
- * and any "hard iron" offsets on the device.
- *
- * @param calibration A Sample3D containing the offsets for the x, y and z axis.
- */
-void MicroBitCompass::setCalibration(CompassCalibration calibration)
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->setCalibration( calibration );
-}
-
-/**
- * Provides the calibration data currently in use by the compass.
- *
- * More specifically, the x, y and z zero offsets of the compass.
- *
- * @return A Sample3D containing the offsets for the x, y and z axis.
- */
-CompassCalibration MicroBitCompass::getCalibration()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->getCalibration();
-}
-
-/**
- * Returns 0 or 1. 1 indicates that the compass is calibrated, zero means the compass requires calibration.
- */
-int MicroBitCompass::isCalibrated()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->isCalibrated();
-}
-
-/**
- * Returns 0 or 1. 1 indicates that the compass is calibrating, zero means the compass is not currently calibrating.
- */
-int MicroBitCompass::isCalibrating()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->isCalibrating();
-}
-
-/**
- * Clears the calibration held in memory storage, and sets the calibrated flag to zero.
- */
-void MicroBitCompass::clearCalibration()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->clearCalibration();
-}
-
-/**
- * Configures the device for the sample rate defined
- * in this object. The nearest values are chosen to those defined
- * that are supported by the hardware. The instance variables are then
- * updated to reflect reality.
- *
- * @return MICROBIT_OK on success, MICROBIT_I2C_ERROR if the compass could not be configured.
- */
 int MicroBitCompass::configure()
 {
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->configure();
+    if ( MicroBitCompass::driver == this )
+    {
+        microbit_panic( MicroBitPanic::COMPASS_ERROR );
+        return DEVICE_NOT_SUPPORTED;
+    }
+    else
+    {
+        // Using an else helps the compiler to compile out this bit if possible
+        return driver->configure();
+    }
 }
 
-/**
- *
- * Defines the accelerometer to be used for tilt compensation.
- *
- * @param acceleromter Reference to the accelerometer to use.
- */
-void MicroBitCompass::setAccelerometer(MicroBitAccelerometer &accelerometer)
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->setAccelerometer( accelerometer );
-}
-
-/**
- * Attempts to set the sample rate of the compass to the specified period value (in ms).
- *
- * @param period the requested time between samples, in milliseconds.
- * @return MICROBIT_OK on success, MICROBIT_I2C_ERROR is the request fails.
- *
- * @note The requested rate may not be possible on the hardware. In this case, the
- * nearest lower rate is chosen.
- *
- * @note This method should be overriden (if supported) by specific magnetometer device drivers.
- */
-int MicroBitCompass::setPeriod(int period)
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->setPeriod( period );
-}
-
-/**
- * Reads the currently configured sample rate of the compass.
- *
- * @return The time between samples, in milliseconds.
- */
-int MicroBitCompass::getPeriod()
-{
-    return driver->getPeriod();
-}
-
-/**
- * Poll to see if new data is available from the hardware. If so, update it.
- * n.b. it is not necessary to explicitly call this funciton to update data
- * (it normally happens in the background when the scheduler is idle), but a check is performed
- * if the user explicitly requests up to date data.
- *
- * @return MICROBIT_OK on success, MICROBIT_I2C_ERROR if the update fails.
- *
- * @note This method should be overidden by the hardware driver to implement the requested
- * changes in hardware.
- */
 int MicroBitCompass::requestUpdate()
 {
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->requestUpdate();
+    if ( MicroBitCompass::driver == this )
+    {
+        microbit_panic( MicroBitPanic::COMPASS_ERROR );
+        return DEVICE_NOT_SUPPORTED;
+    }
+    else
+    {
+        // Using an else helps the compiler to compile out this bit if possible
+        return driver->requestUpdate();
+    }
 }
 
-/**
- * Reads the last compass value stored, and provides it in the coordinate system requested.
- *
- * @param coordinateSpace The coordinate system to use.
- * @return The force measured in each axis, in milli-g.
- */
-Sample3D MicroBitCompass::getSample(CoordinateSystem coordinateSystem)
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->getSample( coordinateSystem );
-}
-
-/**
- * Reads the last compass value stored, and in the coordinate system defined in the constructor.
- * @return The force measured in each axis, in milli-g.
- */
-Sample3D MicroBitCompass::getSample()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->getSample();
-}
-
-/**
- * reads the value of the x axis from the latest update retrieved from the compass,
- * using the default coordinate system as specified in the constructor.
- *
- * @return the force measured in the x axis, in milli-g.
- */
-int MicroBitCompass::getX()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->getX();
-}
-
-/**
- * reads the value of the y axis from the latest update retrieved from the compass,
- * using the default coordinate system as specified in the constructor.
- *
- * @return the force measured in the y axis, in milli-g.
- */
-int MicroBitCompass::getY()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->getY();
-}
-
-/**
- * reads the value of the z axis from the latest update retrieved from the compass,
- * using the default coordinate system as specified in the constructor.
- *
- * @return the force measured in the z axis, in milli-g.
- */
-int MicroBitCompass::getZ()
-{
-    if( driver == NULL )
-        target_panic( MicroBitPanic::COMPASS_ERROR );
-    
-    return driver->getZ();
-}
-
-/**
-  * Destructor.
-  */
 MicroBitCompass::~MicroBitCompass()
 {
 }
-
