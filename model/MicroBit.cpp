8c8db00ea6a13fef49314c4e000e2d3da68a6427
Carlos Pereira Atencio <carlos@microbit.org>
2024-05-21 17:32:43 +0100

    Set uBit.io.logo to capacitive touch mode by default. (#418)
    Without initialising the touch engine in the background.

--- model/MicroBit.cpp
+++ model/MicroBit.cpp
@@ -116,16 +116,13 @@ MicroBit::MicroBit() :
     }
     */
 
-    // Configure serial port for debugging
-
-    //SERIAL_TODO:
-    // serial.set_flow_control(mbed::Serial::Disabled);
-    //serial.baud(115200);
-
     // Enable the serial port as a deep sleep wake event. (Useful for MicroPython REPL, for example)
     #if CONFIG_ENABLED(MICROBIT_USB_SERIAL_WAKE)
         serial.status |= CODAL_SERIAL_STATUS_DEEPSLEEP;
     #endif
+
+    // uBit.logo is a Capacitive TouchButton, ensure the io pin is set capacitive as well
+    io.logo.status |= IO_STATUS_CAPACITATIVE_TOUCH;
 
     // Add pullup resisitor to IRQ line (it's floating ACTIVE LO)
     io.irq1.getDigitalValue();
