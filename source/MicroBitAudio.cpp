3c103ffc9cbd3826814faf9507cf3f5cfaad409e
Joe Finney <joe@comp.lancs.ac.uk>
2025-01-30 11:53:27 +0000

    Increase start up delay to 3 frames, to avoid occasional clicks.

--- source/MicroBitAudio.cpp
+++ source/MicroBitAudio.cpp
@@ -109,7 +109,7 @@ void MicroBitAudio::activateMic(){
     runmic.setDigitalValue(1);
     runmic.setHighDrive(true);
     adc.activateChannel(mic);
-    adc.getChannel(microphone, false)->setStartDelay(2);
+    adc.getChannel(microphone, false)->setStartDelay(3);
     this->micEnabled = true;
 }
 
